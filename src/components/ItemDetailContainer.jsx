a12cdc03706b77bfa04c3607e03018219d231d0e
JuliErre <julian.erreguerena@gmail.com>
2021-12-16 14:22:01 -0300

    cambie en itemdetailContainer a que el producto sea un objeto

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -16,12 +16,12 @@ const getItem = new Promise((resolve, reject) => {
 
 
 function ItemDetailContainer() {
-    const [product, setProduct] = useState([]);
+    const [product, setProduct] = useState({});
     const [loading, setLoading] = useState(true);
     const { id } = useParams();
 
     useEffect(() => {
-        getItem.then(data => setProduct(data.filter(prod => prod.id == id)))
+        getItem.then(data => setProduct(data.find(prod => prod.id == id)))
             .finally(() => setLoading(false))
     }, [id]);
 
@@ -33,7 +33,7 @@ function ItemDetailContainer() {
 
         <div className="itemDetailContainer">
             {loading ? <img src={Loading} alt="Cargando" className="loading" /> :
-                <ItemDetail key={product.id} product={product[0]} />}
+                <ItemDetail key={product.id} product={product} />}
         </div>
 
        
