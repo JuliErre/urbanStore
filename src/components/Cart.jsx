87591395b6a14be8c6eeafe175115140ef85acbd
Julian Erre <julian.erreguerena@gmail.com>
2021-12-26 02:56:16 -0300

    le cambie el estilo al carrito

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,49 +4,59 @@ import { CartContext } from './CartContext'
 import './Cart.css'
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react/cjs/react.development'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
 
 
 function Cart() {
-    const [total,setTotal] = useState()
+    const [total, setTotal] = useState()
 
     const { cartList, clearCart, removeProduct } = useContext(CartContext)
     console.log(CartContext)
-    useEffect(()=> {
-        let totals = 0
-     cartList.forEach(prod=> {
-         totals+=prod.precioTotal
-         
-     });
-     setTotal(totals)
-    }, [cartList])
+     useEffect(() => {
+         let precioTotal = 0
+         cartList.forEach(prod => {
+             precioTotal += prod.precioTotal
 
+         });
+         setTotal(precioTotal)
+     }, [cartList])
+
+ 
     return (
-        <div>
-            <button onClick={clearCart}>Vaciar carrito</button>
-            <p>{total}</p>
+        
+
+
             <div className="cartList">
+                <div className="head">
+                    <div>
+                        <button className="btnBlack" onClick={clearCart}>Vaciar carrito</button>
+                        <button className="btnBlack">Terminar compra</button>
+                    </div>
+                    <h4>Total: $ {total} </h4>
+                </div>
+                
                 {cartList.length ?
                     cartList.map(prod =>
                         <div className="itemCart" key={prod.id}>
                             <img src={prod.img} alt="" />
                             <h3>{prod.name}</h3>
                             <h4>{prod.cantidad}</h4>
-                            <h4> $ {prod.precioTotal} </h4>
-                            <button onClick={() =>removeProduct(prod)}>eliminar</button>
+                            <h4>  $ {prod.precioTotal} </h4>
+                            <button onClick={() => removeProduct(prod)} > <FontAwesomeIcon icon={faTrash} /></button>
                         </div>
                     )
-                    : <div>
+                    
+                    : <div className="carritoVacio">
                         <h3>Carrito Vacio</h3>
                         <Link to={'/productos'} > <button className="btnBlue"> Volver a productos </button> </Link>
                     </div>
                 }
 
-                
-
-
             </div>
-            <button>Terminar compra</button>
-        </div>
+
+        
 
 
     )
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAfejvmjhwr9cFCt8KfCN27ko1TwgyXBts",
+  authDomain: "urbanstore-8ce7a.firebaseapp.com",
+  projectId: "urbanstore-8ce7a",
+  storageBucket: "urbanstore-8ce7a.appspot.com",
+  messagingSenderId: "861568492342",
+  appId: "1:861568492342:web:f543be8b24a958e3548997",
+  measurementId: "G-30S3PMTDCY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
